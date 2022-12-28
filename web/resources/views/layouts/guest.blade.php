74db6fcd2bdbc2cede834bb2cc2dc96c7296088f
谷啟祥 <evan.gu@piapp.com.tw>
2022-12-28 15:53:10 +0800

    fix jetStream page

--- web/resources/views/layouts/guest.blade.php
+++ web/resources/views/layouts/guest.blade.php
@@ -9,8 +9,9 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- Scripts -->
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
