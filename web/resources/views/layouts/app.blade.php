2c29f666770e7bfd024b3b292e8dd8eebc0af49e
谷啟祥 <evan.gu@piapp.com.tw>
2022-12-29 17:11:40 +0800

    add laravel/ui & livewire

--- web/resources/views/layouts/app.blade.php
+++ web/resources/views/layouts/app.blade.php
@@ -10,11 +10,10 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-<script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
         <!-- Styles -->
         @livewireStyles
 
-        <!-- Scripts -->
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
--- web/resources/views/layouts/guest.blade.php
+++ web/resources/views/layouts/guest.blade.php
@@ -10,7 +10,7 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-<script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
         <!-- Scripts -->
     </head>
     <body>
